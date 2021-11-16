a254a5c6974f0777046244d3ffa416c11d494cfa
MrHunte96 <chuahongwei2013@yahoo.com.sg>
2021-11-16 18:03:47 +0800

    Added collision resolution

--- Engine/Utilities.py
+++ Engine/Utilities.py
@@ -4,36 +4,33 @@ from Engine.Vector2 import Vector2
 class CollisionData:
     def __init__(self):
         self.hit = False
-        self.contactPoint = [Vector2()]
+        self.contactPoint = Vector2()
 # Utilities functions
 # +-----> x
 # |
 # v
 # y
-def PointAABB(pt, topleft, bottomright):
+def __PointAABB(pt, topleft, bottomright):
     #data = CollisionData()
     if (pt.x <= topleft.x or pt.y <= topleft.y or 
         pt.x >= bottomright.x or pt.y >= bottomright.y):
         return False
-        #data.hit = False
-    #else:
-        #data.hit = True
-    #return data
     return True
     
-def PointCircle(pt, center, radius):
+def __PointCircle(pt, center, radius):
     return (pt - center).LengthSq() <= radius * radius
 
-def AABBAABB(topleft1, bottomright1, topleft2, bottomright2):
+def __AABBAABB(topleft1, bottomright1, topleft2, bottomright2):
     if (topleft1.x > bottomright2.x or topleft2.x > bottomright1.x or 
         topleft1.y > bottomright2.y or topleft2.y > bottomright1.y):
         return False
     return True
 
-def CircleCircle(c1, r1, c2, r2):
+def __CircleCircle(c1, r1, c2, r2):
     return (c2 - c1).LengthSq() <= (r1 + r2) * (r1 + r2)
 
 def CircleAABB(center, radius, topleft, bottomright):
+    cData = CollisionData()
     topright = Vector2(bottomright.x, topleft.y)
     bottomleft = Vector2(topleft.x, bottomright.y)
     # Expanded Square
@@ -42,31 +39,60 @@ def CircleAABB(center, radius, topleft, bottomright):
     #etopright = topright + Vector2(radius, -radius)
     #ebottomleft = bottomleft + Vector2(-radius, radius)
     
-    if not PointAABB(center, etopleft, ebottomright):
-        return False
-    # Left side
-    if center.x <= topleft.x:
-        # Left Top
-        if center.y <= topleft.y:
-            return PointCircle(center, topleft, radius)
-        # Left Bottom
-        elif center.y >= bottomright.y:
-            return PointCircle(center, bottomleft, radius)
-        # Left
-        else:
-            return True
-    # Right side
-    elif center.x >= bottomright.x:
-        # Right Top
-        if center.y <= topleft.y:
-            return PointCircle(center, topright, radius)
-        # Right Bottom
-        elif center.y >= bottomright.y:
-            return PointCircle(center, bottomright, radius)
-        # Right
-        else:
-            return True
+    if not __PointAABB(center, etopleft, ebottomright):
+        cData.hit = False
     else:
-        return True
+        # Left side
+        if center.x <= topleft.x:
+            # Left Top
+            if center.y <= topleft.y:
+                cData.hit = __PointCircle(center, topleft, radius)
+                cData.contactPoint = topleft
+            # Left Bottom
+            elif center.y >= bottomright.y:
+                cData.hit = __PointCircle(center, bottomleft, radius)
+                cData.contactPoint = bottomleft
+            # Left
+            else:
+                cData.hit = True
+                cData.contactPoint = Vector2(topleft.x, center.y)
+        # Right side
+        elif center.x >= bottomright.x:
+            # Right Top
+            if center.y <= topleft.y:
+                cData.hit = __PointCircle(center, topright, radius)
+                cData.contactPoint = topright
+            # Right Bottom
+            elif center.y >= bottomright.y:
+                cData.hit = __PointCircle(center, bottomright, radius)
+                cData.contactPoint = bottomright
+            # Right
+            else:
+                cData.hit = True
+                cData.contactPoint = Vector2(bottomright.x, center.y)
+        # Top side
+        elif center.y <= topleft.y:
+            cData.hit = True
+            cData.contactPoint = Vector2(center.x, topleft.y)
+        # Bottom side
+        elif center.y >= bottomright.y:
+            cData.hit = True
+            cData.contactPoint = Vector2(center.x, bottomright.y)
+        # Inside
+        else:
+            cData.hit = True
+            # Find closest point to aabb boundary
+            points = [topleft, topright, bottomright, bottomleft]
+            distance = [(topleft-center).LengthSq(), (topright-center).LengthSq(),
+                        (bottomright-center).LengthSq(), (bottomleft-center).LengthSq()]
+            index_min = min(range(len(distance)), key=distance.__getitem__)
+            direction_vector = points[index_min] - center
+            # Check if it's closer to horizontal or vertical
+            if abs(direction_vector.x) < abs(direction_vector.y):
+                cData.contactPoint.x = points[index_min].x
+                cData.contactPoint.y = center.y
+            else:
+                cData.contactPoint.x = center.x
+                cData.contactPoint.y = points[index_min].y
 
-    return False
+    return cData
--- State_Level.py
+++ State_Level.py
@@ -8,23 +8,35 @@ import Engine.Utilities
 import pygame
 import os
 
+class Camera:
+    def __init__(self):
+        self.position = Vector2(0,0)
+
+class Player:
+    def __init__(self):
+        self.position = Vector2(0,0)
+        self.velocity = Vector2(0,0)
+        self.radius = 28
+
+    def colliderData(self):
+        return (self.position + Vector2(32,32), self.radius)
+
 class State_Level(BaseState):
     statename = "Level 1"
 
     def __init__(self, resourcemanager, window):
         super().__init__(resourcemanager, window, State_Level.statename)
         self.backgroundColor = (137, 207, 240)
+        self.gravity = 1.8
 
         self.showDebug = False
-        self.cameraPos = Vector2()
-        self.level = 1
+        self.camera = Camera()
+        
+        self.player = Player()
         self.isOnGround = False
-        self.playerPos = Vector2()
-        self.playerVel = Vector2()
 
         self.levelMap = LevelMap(64) # GridSize = 64x64
-
-        self.tpos = Vector2()
+        self.level = 1
             
     def __drawMap(self):
         Tiles = LevelMap.Tiles
@@ -44,28 +56,46 @@ class State_Level(BaseState):
         for trig in self.levelMap.triggers:
             self.AddDrawDebugRectCall(trig.position, trig.size, YELLOW_COLOR)
 
+        player_collider = self.player.colliderData()
+        self.AddDrawDebugCircleCall(player_collider[0], player_collider[1], GREEN_COLOR)
+
     def __handleCollision(self):
-        pass
+        BLUE_COL = (0,0,255)
+
+        player_collider = self.player.colliderData()
+        # Collision between player and world
+        for colider in self.levelMap.colliders:
+            collision = Engine.Utilities.CircleAABB(player_collider[0], player_collider[1], colider.position, colider.position + colider.size)
+            if collision.hit:
+                # Resolve
+                resolve_dir = (player_collider[0] - collision.contactPoint).Normalized()
+                resolve_dist = self.player.radius - (player_collider[0] - collision.contactPoint).Length()
+                self.player.position += resolve_dir * resolve_dist
+                self.player.velocity = Vector2.Zero()
+                self.isOnGround = True
+                # Debug draw contact point
+                if self.showDebug:
+                    self.AddDrawDebugPointCall(collision.contactPoint, BLUE_COL)
 
     def __handlePhysics(self, dt: float):
         # Gravity
         if not self.isOnGround:
-            self.playerVel.y += 9.8 * dt * 2
+            self.player.velocity.y += self.gravity * dt * 2
             # Terminal velocity
-            if self.playerVel.y > 33.0:
-                self.playerVel.y = 33.0
+            if self.player.velocity.y > 33.0:
+                self.player.velocity.y = 33.0
         else:
-            self.playerVel.y = 0.0
+            self.player.velocity.y = 0.0
 
         # Friction
-        if self.playerVel.x > 0.3:
-            self.playerVel.x -= 0.2
-        elif self.playerVel.x < -0.3:
-            self.playerVel.x += 0.2
+        if self.player.velocity.x > 0.3:
+            self.player.velocity.x -= 0.2
+        elif self.player.velocity.x < -0.3:
+            self.player.velocity.x += 0.2
         else:
-            self.playerVel.x = 0.0
+            self.player.velocity.x = 0.0
 
-        #self.playerPos += self.playerVel * 64.0 * dt # assume 64px = 1metre
+        self.player.position += self.player.velocity * 64.0 * dt # assume 64px = 1metre
 
     def __handleKeyInput(self):
         # Trigger once
@@ -75,67 +105,33 @@ class State_Level(BaseState):
                 if env.key == K_F1:
                     self.showDebug = not self.showDebug
                 if env.key == K_UP and self.isOnGround:
-                    self.playerVel.y = -7.5
+                    self.player.velocity.y = -7.5
         #elif keypress[K_DOWN]:
         #    self.playerVel.y = 5
 
         # Repeated call
         keypress = pygame.key.get_pressed()
         if keypress[K_LEFT]:
-            if self.playerVel.x > -3:
-                self.playerVel.x -= 1
+            if self.player.velocity.x > -3:
+                self.player.velocity.x -= 1
         elif keypress[K_RIGHT]:
-            if self.playerVel.x < 3:
-                self.playerVel.x += 1
+            if self.player.velocity.x < 3:
+                self.player.velocity.x += 1
 
     def Load(self):
         super().Load()
         self.levelMap.LoadMap(os.path.join("Assets", "Level", f'Level{self.level}.dat'))
         self.levelMap.GenerateColliders()
-        self.playerPos = self.levelMap.GetStartPoint_ScreenPos() - Vector2(0,64)
+        self.player.position = self.levelMap.GetStartPoint_ScreenPos() - Vector2(0,64)
 
     def Update(self, dt):
         self.__handleKeyInput()
-        self.__handleCollision()
         self.__handlePhysics(dt)
+        self.__handleCollision()
 
         self.__drawMap()
-        super().AddDrawCall("Ball", self.playerPos)
+        super().AddDrawCall("Ball", self.player.position)
 
-        RED_COL = (255,0,0)
-        RED2_COL = (255,128,128)
-        keypress = pygame.key.get_pressed()
-        if keypress[K_UP]:
-            self.tpos.y -= 1
-        elif keypress[K_DOWN]:
-            self.tpos.y += 1
-        if keypress[K_LEFT]:
-            self.tpos.x -= 1
-        elif keypress[K_RIGHT]:
-            self.tpos.x += 1
-        squarePoint = Vector2(300,300)
-        squareDim = Vector2(200,150)
-        circlePoint = self.tpos
-        circleRadius = 100
-        collision = Engine.Utilities.CircleAABB(circlePoint, circleRadius,squarePoint, squarePoint + squareDim)
-        Debug.Warn(f"Check Collision : {collision}")
-        self.AddDrawDebugRectCall(squarePoint, squareDim, RED_COL)
-        self.AddDrawDebugRectCall(squarePoint - Vector2(circleRadius, circleRadius), 
-                                  squareDim + (Vector2(circleRadius, circleRadius) * 2), RED2_COL)
-        self.AddDrawDebugCircleCall(circlePoint, circleRadius, RED_COL)
-        self.AddDrawDebugPointCall(circlePoint, RED_COL)
-        self.AddDrawDebugCircleCall(squarePoint, circleRadius, RED2_COL)
-        self.AddDrawDebugCircleCall(squarePoint + squareDim, circleRadius, RED2_COL)
-        self.AddDrawDebugCircleCall(squarePoint + Vector2(squareDim.x, 0), circleRadius, RED2_COL)
-        self.AddDrawDebugCircleCall(squarePoint + Vector2(0, squareDim.y), circleRadius, RED2_COL)
-        """
-        squarePoint = Vector2(300,300)
-        squareDim = Vector2(200,150)
-        collision = Engine.Utilities.PointAABB(self.tpos, squarePoint, squarePoint + squareDim)
-        Debug.Warn(f"Check Collision : {collision.hit} | Contact Point : {collision.contactPoint[0]}")
-        self.AddDrawDebugRectCall(squarePoint, squareDim, RED_COL)
-        self.AddDrawDebugPointCall(self.tpos, RED_COL)
-        """
 
         if self.showDebug:
             self.__drawColliders()
